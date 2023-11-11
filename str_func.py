c32e15c7e2a462af57b58176ecfb259c1162c410
Denis Krishchuk <ms.den.best@mail.ru>
2023-11-12 01:32:10 +0500

    add builtins

--- str_func.py
+++ str_func.py
@@ -1,8 +1,8 @@
-def upper_word(word: str):
+def upper_word(word: str) -> str:
     """Возвращает строку, где все буквы заглавные"""
     return word.upper()
 
 
-def capitalize_word(word: str):
+def capitalize_word(word: str) -> str:
     """Возвращает строку, где все первые буквы слов заглавные"""
     return word.capitalize()
